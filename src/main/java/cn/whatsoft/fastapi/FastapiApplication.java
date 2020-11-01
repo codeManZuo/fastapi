bb9eed6a5c33bed6a9a4fb7f846efbeb4d675950
hui <zh@whatsoft.cn>
2020-11-01 17:56:40 +0800

    init

---
+++ src/main/java/cn/whatsoft/fastapi/FastapiApplication.java
@@ -0,0 +1,13 @@
+package cn.whatsoft.fastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastapiApplication.class, args);
+    }
+
+}
